1224c82995011f1d62c2305e6b75a2fb554d4470
PaulDev97 <paul.devc@gmail.com>
2023-07-10 15:39:04 -0300

    z

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <header className='w-full h-auto absolute bottom-5 flex justify-center '>
-      <nav className='text-white w-[60%] flex justify-around items-center gap-6 text-xl rounded-lg  bg-[#111] p-4 '>
+    <header className='w-full absolute bottom-5 flex flex-col items-center justify-end '>
+      <nav className='text-white w-[60%] h-[70px] flex justify-around items-center gap-6 text-xl rounded-lg  bg-[#111] p-4 '>
         <FontAwesomeIcon icon={faUser} />
         <FontAwesomeIcon icon={faLock} />
         <FontAwesomeIcon icon={faPlus} />
